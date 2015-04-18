d005873fe4f8b0a316e3673b4c29d7c5e58f7bb2
jmrepetti <jmrepetti@gmail.com>
2015-04-18 16:59:18 +0200

    Initial Commit

---
+++ lib/date_time_step_with.rb
@@ -0,0 +1,7 @@
+require "date_time_step_with/version"
+
+module DateTimeStepWith
+  # Your code goes here...
+  autoload :CronMatcher, "date_time_step_with/cron_matcher"
+  
+end
---
+++ lib/date_time_step_with/version.rb
@@ -0,0 +1,3 @@
+module DateTimeStepWith
+  VERSION = "0.0.1"
+end
