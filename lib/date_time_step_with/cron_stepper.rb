507f9cb03984f3cdf63743f757ed6ef47660c966
jmrepetti <jmrepetti@gmail.com>
2015-04-18 20:07:00 +0200

    Pass step parameter

--- lib/date_time_step_with/cron_stepper.rb
+++ lib/date_time_step_with/cron_stepper.rb
@@ -1,7 +1,7 @@
 module DateTimeStepWith
   module CronStepper
-    def step_with_cron(cron_expression, limit)
-      self.step(limit).select{|d| d.match_cron?(cron_expression) }.each
+    def step_with_cron(cron_expression, limit,step=1)
+      self.step(limit,step).select{|d| d.match_cron?(cron_expression) }.each
     end
   end
 end
