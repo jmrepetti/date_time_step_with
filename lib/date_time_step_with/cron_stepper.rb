d19119c7e660fbc3fa4a81fb073e32a253ec890c
jmrepetti <jmrepetti@gmail.com>
2015-05-01 15:39:11 +0200

    Step on minutes if allowed

--- lib/date_time_step_with/cron_stepper.rb
+++ lib/date_time_step_with/cron_stepper.rb
@@ -2,6 +2,14 @@ module DateTimeStepWith
   module CronStepper
     def step_with_cron(cron_expression, limit,step=1)
       collection = self.step(limit,step).select{|d| d.match_cron?(cron_expression) }
+    def step_with_cron(cron_expression, limit,step=nil, &block)
+
+      #if no step given, then iterate on the minimum cron value
+      if step.nil? && self.respond_to?(:minute)
+        step = (1.to_f/24/60) #one minute
+      else
+        step = 1#one day
+      end
       if block_given?
         collection.each do |d|
           yield d
