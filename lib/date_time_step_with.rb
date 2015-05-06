fdd8333a65934952967eec47d6195bfa8193446d
jmrepetti <jmrepetti@gmail.com>
2015-05-06 07:29:44 +0200

    Moved Stepper and Matcher to extensions folder

--- lib/date_time_step_with.rb
+++ lib/date_time_step_with.rb
@@ -2,7 +2,7 @@ require "date_time_step_with/version"
 
 module DateTimeStepWith
 
-  autoload :CronMatcher, "date_time_step_with/cron_matcher"
-  autoload :CronStepper, "date_time_step_with/cron_stepper"
+  autoload :CronMatcher, "date_time_step_with/extensions/cron_matcher"
+  autoload :CronStepper, "date_time_step_with/extensions/cron_stepper"
   autoload :CronExpression, "date_time_step_with/cron_expression"
 end
